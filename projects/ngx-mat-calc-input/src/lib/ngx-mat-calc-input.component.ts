3bf5e64290a73fef76d368161f6309e43535f077
carlos <carlos@takeapps.com>
2020-05-21 02:41:35 -0300

    Added sample material component

--- projects/ngx-mat-calc-input/src/lib/ngx-mat-calc-input.component.ts
+++ projects/ngx-mat-calc-input/src/lib/ngx-mat-calc-input.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'ngx-mat-calc-input',
   template: `
     <p>
-      ngx-mat-calc-input works!
+      <mat-label>ngx-mat-calc-input works with material!</mat-label>
     </p>
   `,
   styles: [
--- projects/ngx-mat-calc-input/src/lib/ngx-mat-calc-input.module.ts
+++ projects/ngx-mat-calc-input/src/lib/ngx-mat-calc-input.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { NgxMatCalcInputComponent } from './ngx-mat-calc-input.component';
-
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 
 @NgModule({
-  declarations: [NgxMatCalcInputComponent],
+  declarations: [
+    NgxMatCalcInputComponent,
+    
+  ],
   imports: [
+    MatFormFieldModule
   ],
   exports: [NgxMatCalcInputComponent]
 })
