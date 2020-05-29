99b60d68b816a67aa1a3f5deb5527acb9bfb2b5b
Carlos <carlos@takeapps.com>
2020-05-29 18:29:28 -0300

    App tests fixed (component pending)

--- projects/ngx-mat-calc-input/src/lib/ngx-mat-calc-input.service.spec.ts
+++ projects/ngx-mat-calc-input/src/lib/ngx-mat-calc-input.service.spec.ts
@@ -1,16 +1,34 @@
 import { TestBed } from '@angular/core/testing';
 
 import { NgxMatCalcInputService } from './ngx-mat-calc-input.service';
+import { ReactiveFormsModule, FormsModule, FormBuilder } from '@angular/forms';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('NgxMatCalcInputService', () => {
   let service: NgxMatCalcInputService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(NgxMatCalcInputService);
+    TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule,
+        FormBuilder,
+        FormsModule
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    });
+    service = new NgxMatCalcInputService();
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+  // it('should be created', () => {
+  //   expect(service).toBeTruthy();
+  // });
+
+  it('Test initial value to be 0', (done: DoneFn) => {
+    let value = service.Value.subscribe(value => {
+      expect(value).toBe(0);
+      done();
+    });
+  })
+
+
 });
