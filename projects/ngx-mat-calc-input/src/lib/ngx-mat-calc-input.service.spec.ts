ec688e6630cc0c7b1fc4b58b3daa1777e969bff8
carlos <carlos@takeapps.com>
2020-05-30 00:00:43 -0300

    added service unit tests

--- projects/ngx-mat-calc-input/src/lib/ngx-mat-calc-input.service.spec.ts
+++ projects/ngx-mat-calc-input/src/lib/ngx-mat-calc-input.service.spec.ts
@@ -3,6 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { NgxMatCalcInputService } from './ngx-mat-calc-input.service';
 import { ReactiveFormsModule, FormsModule, FormBuilder } from '@angular/forms';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { CalcOperator } from './ngx-mat-calc-input.enums';
 
 describe('NgxMatCalcInputService', () => {
   let service: NgxMatCalcInputService;
@@ -23,5 +24,102 @@ describe('NgxMatCalcInputService', () => {
     });
   })
 
+  it('Test simple addition', (done: DoneFn) => {
+    service.EnterDigit(2);
+    service.EnterOperator(CalcOperator.Add);
+    service.EnterDigit(2);
+    service.EnterOperator(CalcOperator.Equals);
 
+    let value = service.Value.subscribe(value => {
+      expect(value).toBe(4); 
+      done();
+    });
+  })
+
+  it('Test multiple operations', (done: DoneFn) => {
+    service.EnterDigit(2);
+    service.EnterOperator(CalcOperator.Add);
+    service.EnterDigit(2);
+    service.EnterOperator(CalcOperator.Substract);
+    service.EnterDigit(5);
+    
+    service.EnterOperator(CalcOperator.Equals);
+
+    let value = service.Value.subscribe(value => {
+      expect(value).toBe(-1); 
+      done();
+    });
+  })
+
+  it('Test use equal between two operations', (done: DoneFn) => {
+    service.EnterDigit(2);
+    service.EnterOperator(CalcOperator.Add);
+    service.EnterDigit(2);
+    service.EnterOperator(CalcOperator.Equals);
+    service.EnterOperator(CalcOperator.Substract);
+    service.EnterDigit(5);
+    
+    service.EnterOperator(CalcOperator.Equals);
+
+    let value = service.Value.subscribe(value => {
+      expect(value).toBe(-1); 
+      done();
+    });
+  })
+
+  it('Test use equal multiple times', (done: DoneFn) => {
+    service.EnterDigit(4);
+    service.EnterOperator(CalcOperator.Add);
+    service.EnterDigit(1);
+    service.EnterOperator(CalcOperator.Equals);    
+    service.EnterOperator(CalcOperator.Equals);
+    service.EnterOperator(CalcOperator.Equals);
+
+    let value = service.Value.subscribe(value => {
+      expect(value).toBe(7); 
+      done();
+    });
+  })
+
+  it('Test use clear', (done: DoneFn) => {
+    service.EnterDigit(4);
+    service.EnterOperator(CalcOperator.Add);
+    service.ResetContext();
+    
+    let value = service.Value.subscribe(value => {
+      expect(value).toBe(0); 
+      done();
+    });
+  })
+
+  it('Test use clear and try again', (done: DoneFn) => {
+    service.EnterDigit(4);
+    service.EnterOperator(CalcOperator.Add);
+    service.EnterDigit(2);
+
+    service.ResetContext();
+    
+    service.EnterDigit(1);
+    service.EnterOperator(CalcOperator.Add);
+    service.EnterDigit(8);
+    service.EnterOperator(CalcOperator.Equals);
+
+    let value = service.Value.subscribe(value => {
+      expect(value).toBe(9); 
+      done();
+    });
+  })
+
+  it('Test division by zero', (done: DoneFn) => {
+    service.EnterDigit(3);
+    service.EnterOperator(CalcOperator.Divide);
+    service.EnterDigit(0);
+    service.EnterOperator(CalcOperator.Equals);
+
+    let value = service.Value.subscribe(value => {
+      expect(value).toBe(Infinity); 
+      done();
+    });
+  })
+  
 });
