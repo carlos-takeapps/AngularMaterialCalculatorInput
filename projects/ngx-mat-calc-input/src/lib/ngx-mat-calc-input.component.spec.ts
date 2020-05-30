4b3aa32528ff27866955cdb854dcd2919355dc25
carlos <carlos@takeapps.com>
2020-05-30 00:26:03 -0300

    adding ui tests

--- projects/ngx-mat-calc-input/src/lib/ngx-mat-calc-input.component.spec.ts
+++ projects/ngx-mat-calc-input/src/lib/ngx-mat-calc-input.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NgxMatCalcInputComponent } from './ngx-mat-calc-input.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('NgxMatCalcInputComponent', () => {
   let component: NgxMatCalcInputComponent;
@@ -31,4 +32,41 @@ describe('NgxMatCalcInputComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display Calc button', () => {
+    const fixture = TestBed.createComponent(NgxMatCalcInputComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.keyboard-button').hasAttribute('hidden')).toEqual(false);
+  });
+
+  it('should display keyboard', () => {
+    const fixture = TestBed.createComponent(NgxMatCalcInputComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+
+    let divButton = fixture.debugElement.query(By.css('keyboard-button'));
+
+    divButton.triggerEventHandler('click', {});
+    fixture.detectChanges();
+
+    expect(compiled.querySelector('.calc-keyboard').hasAttribute('hidden')).toEqual(false);
+  });
+
+  // it('should hide keyboard', () => {
+  //   const fixture = TestBed.createComponent(NgxMatCalcInputComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement;
+  //   compiled.querySelector('.keyboard-button').nativeElement.click();
+  //   expect(compiled.querySelector('.calc-keyboard').hasAttribute('hidden')).toEqual(false);
+  // });
+
+  // it('should update value', () => {
+  //   const fixture = TestBed.createComponent(NgxMatCalcInputComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement;
+  //   compiled.querySelector('.keyboard-button').nativeElement.click();
+  //   expect(compiled.querySelector('.calc-keyboard').hasAttribute('hidden')).toEqual(false);
+  // });
+
 });
