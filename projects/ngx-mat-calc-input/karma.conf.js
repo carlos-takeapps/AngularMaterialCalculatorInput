5f377e7680c6d4c735a10614e0000854fb4e8b35
Carlos @ TakeApps <42281742+carlos-takeapps@users.noreply.github.com>
2020-06-01 03:20:01 -0300

    Update karma.conf.js

--- projects/ngx-mat-calc-input/karma.conf.js
+++ projects/ngx-mat-calc-input/karma.conf.js
@@ -28,15 +28,12 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['ChromeHeadlessCustom'],
     singleRun: false,
-    restartOnFileChange: true
-  },
+    restartOnFileChange: true,
   customLaunchers: {
   ChromeHeadlessCustom: {
     base: 'ChromeHeadless',
     flags: ['--no-sandbox', '--disable-gpu']
   }
-},
-            
-            
-            );
+  }     
+  });
 };
