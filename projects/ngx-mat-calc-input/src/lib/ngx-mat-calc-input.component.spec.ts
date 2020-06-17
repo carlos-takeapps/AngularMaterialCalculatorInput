bdeef8e517573680220f9a413e23b677e31fe5fe
Carlos @ TakeApps <carlos@TakeApps.com>
2020-06-17 02:24:32 -0300

    Simplified structure Updated publish version Fixed UI Unit Tests

--- projects/ngx-mat-calc-input/src/lib/ngx-mat-calc-input.component.spec.ts
+++ projects/ngx-mat-calc-input/src/lib/ngx-mat-calc-input.component.spec.ts
@@ -1,25 +1,39 @@
-import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NgxMatCalcInputComponent } from './ngx-mat-calc-input.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
+import { FaIconComponent } from "@fortawesome/angular-fontawesome";
+import { Subject } from 'rxjs/internal/Subject';
+import { FocusMonitor } from '@angular/cdk/a11y';
+
 describe('NgxMatCalcInputComponent', () => {
   let component: NgxMatCalcInputComponent;
   let fixture: ComponentFixture<NgxMatCalcInputComponent>;
+  let focusMonitorObservable: Subject<string>;
 
   beforeEach(async(() => {
+    const fmSpy = jasmine.createSpyObj('FocusMonitor', ['monitor']);
+
     TestBed.configureTestingModule({
       imports: [
         FormsModule,
         ReactiveFormsModule,
       ],
       declarations: [
-        NgxMatCalcInputComponent
+        NgxMatCalcInputComponent,
+        FaIconComponent
+      ],
+      providers: [
+        { provide: FocusMonitor, useValue: fmSpy },
       ],
       schemas: [NO_ERRORS_SCHEMA]
     })
       .compileComponents();
+    let focusMonitorSpy = TestBed.get(FocusMonitor);
+    focusMonitorObservable = new Subject<string>();
+    focusMonitorSpy.monitor.and.returnValue(focusMonitorObservable);
   }));
 
   beforeEach(() => {
@@ -28,10 +42,10 @@ describe('NgxMatCalcInputComponent', () => {
     fixture.detectChanges();
   });
 
- it('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
- 
+
   it('should display Calc button', () => {
     const fixture = TestBed.createComponent(NgxMatCalcInputComponent);
     fixture.detectChanges();
@@ -40,37 +54,40 @@ describe('NgxMatCalcInputComponent', () => {
 
     expect(compiled.querySelector('.keyboard-button').hasAttribute('hidden')).toEqual(false);
   });
- 
-/* 
-  it('should display keyboard', <any>fakeAsync(() => {
+
+  it('should display keyboard', () => {
     const fixture = TestBed.createComponent(NgxMatCalcInputComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
 
+    focusMonitorObservable.next('mouse');
+
     compiled.querySelector('.keyboard-button').click();
     fixture.detectChanges();
 
-    console.log(compiled);
-
     expect(compiled.querySelector('.calc-keyboard').hasAttribute('hidden')).toEqual(false);
-  }));
+  });
 
   it('should hide keyboard', () => {
     const fixture = TestBed.createComponent(NgxMatCalcInputComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
 
+    focusMonitorObservable.next('mouse');
+
     compiled.querySelector('.keyboard-button').click();
     compiled.querySelector('.keyboard-button').click();
     fixture.detectChanges();
 
-    expect(compiled.querySelector('.calc-keyboard')===null).toBeTrue();
-  }); 
-  
-  it('should update value', <any>fakeAsync(() => {
+    expect(compiled.querySelector('.calc-keyboard') === null).toBeTrue();
+  });
+
+  it('should update value', () => {
     const fixture = TestBed.createComponent(NgxMatCalcInputComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
+
+    focusMonitorObservable.next('mouse');
 
     compiled.querySelector('.keyboard-button').click();
     fixture.detectChanges();
@@ -78,15 +95,16 @@ describe('NgxMatCalcInputComponent', () => {
     compiled.querySelector('#btn_2').click();
     fixture.detectChanges();
 
-    tick(3000);
-    
-    expect(compiled.querySelector('.input-display').innerHTML).toBe("2");
-  }));
- 
-  it('should resolve calculation', <any>fakeAsync(() => {
+    const nodeAttributes = compiled.querySelector('.input-display').attributes as NamedNodeMap;
+    expect(nodeAttributes.getNamedItem('ng-reflect-model').value).toEqual("2");
+  });
+
+  it('should resolve calculation', () => {
     const fixture = TestBed.createComponent(NgxMatCalcInputComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
+
+    focusMonitorObservable.next('mouse');
 
     compiled.querySelector('.keyboard-button').click();
     fixture.detectChanges();
@@ -95,9 +113,9 @@ describe('NgxMatCalcInputComponent', () => {
     compiled.querySelector('#btn_add').click();
     compiled.querySelector('#btn_5').click();
     compiled.querySelector('#btn_eq').click();
-    tick();
     fixture.detectChanges();
 
-    expect(compiled.querySelector('.input-display').value).toBe("7");
-  })); */
+    const nodeAttributes = compiled.querySelector('.input-display').attributes as NamedNodeMap;
+    expect(nodeAttributes.getNamedItem('ng-reflect-model').value).toEqual("7");
+  });
 });
